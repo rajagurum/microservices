dc6398ab38a07117d970a7409b6cc7576e22bd45
Jeyanthi2208 <jeyanthi2208@gmail.com>
2019-01-09 12:42:29 +0530

    removed

--- src/main/java/com/in28minutes/microservices/springcloudconfigserver/SpringCloudConfigServerApplication.java
+++ src/main/java/com/in28minutes/microservices/springcloudconfigserver/SpringCloudConfigServerApplication.java
@@ -3,8 +3,10 @@ package com.in28minutes.microservices.springcloudconfigserver;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @EnableConfigServer
+@EnableEurekaClient
 @SpringBootApplication
 public class SpringCloudConfigServerApplication {
 
